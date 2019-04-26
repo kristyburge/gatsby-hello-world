a344b40b99eca7d0dc38e15287cb99c342b10e96
Kristy Burge <kristyburge@gmail.com>
2019-04-26 10:29:48 -0500

    Clean up contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,11 +9,9 @@ const ContactPage = () => {
 			<Head title="Contact" />
 			<h1>Contact</h1>
 			<address>
-				1234 Main Street
-				<br />
-				Oklahoma City, OK 73127
+				Oklahoma City, Oklahoma
 			</address>
-			<p>Phone: 555-123-4567</p>
+			<p><em>Phone:</em> 555-123-4567</p>
 
 			<p>Find me on <a href="https://github.com/kristyburge" target="_blank" rel="noopener noreferrer">GitHub</a></p>
 			<p>Connect with me <a href="https://twitter.com/kristyburge" target="_blank" rel="noopener noreferrer">@kristyburge</a> on Twitter.</p>
